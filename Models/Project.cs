8aadcd21b64e50ac68c7c48d78df4f69eb951d3b
ameena <ameena4794@gmail.com>
2020-07-11 23:10:10 +0530

    Add project files.

---
+++ Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreProject_Ameena.Models
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string UnitType { get; set; }
+        public int Rate { get; set; }
+
+
+    }
+}
